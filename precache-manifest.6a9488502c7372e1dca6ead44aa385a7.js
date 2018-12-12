aecaaf6663f754246b0aa886bef4b2b8a1c6cbcd
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-12-12 10:43:25 -0800

    Updates

---
+++ precache-manifest.6a9488502c7372e1dca6ead44aa385a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "067536269b53532b93eaeec55cb32b25",
+    "url": "/tamagotchi-deploy/static/media/tamashell.06753626.png"
+  },
+  {
+    "revision": "4241e9bdd31f982b12e8f6bee9caf1a8",
+    "url": "/tamagotchi-deploy/static/media/logo.4241e9bd.png"
+  },
+  {
+    "revision": "769770686269e66e4037",
+    "url": "/tamagotchi-deploy/static/js/runtime~main.76977068.js"
+  },
+  {
+    "revision": "e4fc3b4788f3280e7b2c",
+    "url": "/tamagotchi-deploy/static/js/main.e4fc3b47.chunk.js"
+  },
+  {
+    "revision": "4a4d1fac52a0e9f556e1",
+    "url": "/tamagotchi-deploy/static/js/1.4a4d1fac.chunk.js"
+  },
+  {
+    "revision": "4a4d1fac52a0e9f556e1",
+    "url": "/tamagotchi-deploy/static/css/1.3e2e366e.chunk.css"
+  },
+  {
+    "revision": "066b8206d48a19e69dbe0c52b5383a90",
+    "url": "/tamagotchi-deploy/index.html"
+  }
+];
